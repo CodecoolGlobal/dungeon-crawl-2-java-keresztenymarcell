999c3527cb5eeb8b8394dadb38e1e5e8c3edf58a
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-10-12 11:27:09 +0200

    Feat: add inventory list, getter and setter, addItemToInventory() and removeItemFromInventory() methods to Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,10 +1,33 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Item;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Player extends Actor {
+
+    private List<Item> inventory = new LinkedList<>();
+
     public Player(Cell cell) {
         super(cell);
+    }
+
+    public void addItemToInventory(Item item) {
+        inventory.add(item);
+    }
+
+    public void removeItemFromInventory(Item item) {
+        inventory.add(item);
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Item> inventory) {
+        this.inventory = inventory;
     }
 
     public String getTileName() {
