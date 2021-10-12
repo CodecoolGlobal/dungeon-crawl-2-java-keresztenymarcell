45cb3c84f383135b17c5a9d4913763e5c9e8de5e
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-10-12 11:50:23 +0200

    feat: add move() overriding and implement pick up key in Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,7 +1,9 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.Key;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,6 +14,21 @@ public class Player extends Actor {
 
     public Player(Cell cell) {
         super(cell);
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = cell.getNeighbor(dx, dy);
+        if(nextCell.getType() != CellType.WALL && !(nextCell.getActor() instanceof Monster)){
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        } else if (nextCell.getItem() != null && nextCell.getItem() instanceof Key) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            addItemToInventory(nextCell.getItem());
+            cell = nextCell;
+        }
     }
 
     public void addItemToInventory(Item item) {
