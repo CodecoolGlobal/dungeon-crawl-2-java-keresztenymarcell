efdf36965cfbb8fa76420b0595e0f5c91dfdac9e
Vincze Gábor <cantataprofana1930@gmail.com>
2021-10-12 11:23:24 +0200

    feat: implement that player can attack monsters, and monsters attack back

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -16,10 +16,23 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if(nextCell.getType() != CellType.WALL && !(nextCell.getActor() instanceof Monster)){
+        if(nextCell.getType() != CellType.WALL && nextCell.getActor() == null){
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
+        }
+        else if(this instanceof Player && nextCell.getActor() instanceof Monster){
+            attack(cell, nextCell);
+        }
+    }
+
+    void attack(Cell cell, Cell nextCell){
+        Actor enemy = nextCell.getActor();
+        enemy.setHealth(enemy.getHealth() - this.getAttack());
+        if(enemy.getHealth() <= 0)
+            nextCell.setActor(null);
+        else{
+            this.setHealth(this.getHealth() - enemy.getAttack());
         }
     }
 
