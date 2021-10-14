e08e66eaa79b8da97b93ca6de4372014559c986f
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-10-14 14:54:10 +0200

    fix: modify health tile in Tiles class

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -43,7 +43,7 @@ public class Tiles {
         tileMap.put("openDoor" , new Tile(10,11));
         tileMap.put("apple", new Tile(15, 29));
         tileMap.put("inventory", new Tile(15, 26));
-        tileMap.put("health", new Tile(25, 12));
+        tileMap.put("health", new Tile(23, 22));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
