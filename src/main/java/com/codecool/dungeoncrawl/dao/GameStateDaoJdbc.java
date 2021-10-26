f88444af5b76ca988a790ea632d2991f73f5715d
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-10-26 17:43:14 +0200

    feat: add discovered_maps handling to methods in GameStateDaoJdbc class

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -6,6 +6,7 @@ import com.codecool.dungeoncrawl.model.PlayerModel;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
@@ -21,12 +22,13 @@ public class GameStateDaoJdbc implements GameStateDao {
     public void add(GameState state) {
         try (Connection conn = dataSource.getConnection()) {
 
-            String sql = "INSERT INTO game_state (id, current_map, saved_at, player_id) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO game_state (id, current_map, discovered_maps, saved_at, player_id) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             st.setInt(1, state.getId());
             st.setString(2, state.getCurrentMap());
-            st.setDate(3, state.getSavedAt());
-            st.setInt(4, state.getPlayer().getId());
+            st.setString(3, String.join(", ", state.getDiscoveredMaps()));
+            st.setDate(4, state.getSavedAt());
+            st.setInt(5, state.getPlayer().getId());
             st.executeUpdate();
             ResultSet rs = st.getGeneratedKeys();
             rs.next();
@@ -39,12 +41,13 @@ public class GameStateDaoJdbc implements GameStateDao {
     @Override
     public void update(GameState state) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "UPDATE game_state SET id = ?, current_map = ?, saved_at = ? WHERE player_id = ?";
+            String sql = "UPDATE game_state SET id = ?, current_map = ?, discovered_maps = ?, saved_at = ? WHERE player_id = ?";
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, state.getId());
             st.setString(2, state.getCurrentMap());
-            st.setDate(3, state.getSavedAt());
-            st.setInt(4, state.getPlayer().getId());
+            st.setString(3, String.join(", ", state.getDiscoveredMaps()));
+            st.setDate(4, state.getSavedAt());
+            st.setInt(5, state.getPlayer().getId());
             st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -55,7 +58,7 @@ public class GameStateDaoJdbc implements GameStateDao {
     public GameState get(int id) {      // id = player_id
         try (Connection conn = dataSource.getConnection()) {
             // FIRST STEP - read current_map and saved at according to player_id
-            String sql = "SELECT id, current_map, saved_at FROM game_state WHERE player_id = ?";
+            String sql = "SELECT id, current_map, discovered_maps, saved_at FROM game_state WHERE player_id = ?";
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
@@ -65,13 +68,15 @@ public class GameStateDaoJdbc implements GameStateDao {
 
             int gameStateId = rs.getInt(1);
             String currentMap = rs.getString(2);
-            Date savedAt = rs.getDate(3);
+            List<String> discoveredMaps = Arrays.asList(rs.getString(3).split(", ", -1));
+            Date savedAt = rs.getDate(4);
 
             // SECOND STEP - find Player with id we got as a result of the first query
             PlayerModel player = playerDao.get(id);
 
             // FINISH - create and return new GameState class instance
             GameState gameState = new GameState(currentMap, savedAt, player);
+            discoveredMaps.forEach(gameState::addDiscoveredMap);
             gameState.setId(gameStateId);
             return gameState;
         } catch (SQLException e) {
@@ -83,7 +88,7 @@ public class GameStateDaoJdbc implements GameStateDao {
     public List<GameState> getAll() {
         try (Connection conn = dataSource.getConnection()) {
             // FIRST STEP - read book_id, author_id and title
-            String sql = "SELECT id, current_map, saved_at, player_id FROM game_state";
+            String sql = "SELECT id, current_map, discovered_maps, saved_at, player_id FROM game_state";
             ResultSet rs = conn.createStatement().executeQuery(sql);
 
             List<GameState> result = new ArrayList<>();
@@ -91,14 +96,16 @@ public class GameStateDaoJdbc implements GameStateDao {
                 // SECOND STEP - read all data from result set
                 int gameStateId = rs.getInt(1);
                 String currentMap = rs.getString(2);
-                Date savedAt = rs.getDate(3);
-                int playerId = rs.getInt(4);
+                List<String> discoveredMaps = Arrays.asList(rs.getString(3).split(", ", -1));
+                Date savedAt = rs.getDate(4);
+                int playerId = rs.getInt(5);
 
                 // THIRD STEP - find player with id == playerId
                 PlayerModel player = playerDao.get(playerId);
 
                 // FOURTH STEP - create a new GameState class instance and add it to result list.
                 GameState gameState = new GameState(currentMap, savedAt, player);
+                discoveredMaps.forEach(gameState::addDiscoveredMap);
                 gameState.setId(gameStateId);
                 result.add(gameState);
             }
